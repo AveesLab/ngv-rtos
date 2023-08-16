50da9feb971e16d6bfbdc0c6d19bbf3c7f7dd142
unknown <yongse6199@gmail.com>
2023-08-16 15:37:12 +0900

    fix 01

--- 01. Hello World/master.c
+++ 01. Hello World/master.c
@@ -5,14 +5,14 @@ extern App_AsclinAsc g_AsclinAsc;
 
 ISR(asclin0TxISR)
 {
-	IfxAsclin_Asc_isrTransmit(&g_AsclinAsc.drivers.asc);
+    IfxAsclin_Asc_isrTransmit(&g_AsclinAsc.drivers.asc);
 }
 
 
 TASK(Task1)
 {
-	printfSerial("Hello World\n");
-	TerminateTask();
+    printfSerial("Hello World\n");
+    TerminateTask();
 }
 
 
