7fd9a52017cc1e3a8493e95d6ad4fea0b7ac2bc7
unknown <yongse6199@gmail.com>
2023-08-16 15:44:40 +0900

    fix indentation

--- 05. Tasks/master.c
+++ 05. Tasks/master.c
@@ -5,30 +5,30 @@ extern App_AsclinAsc g_AsclinAsc;
 
 ISR(asclin0TxISR)
 {
-	IfxAsclin_Asc_isrTransmit(&g_AsclinAsc.drivers.asc);
+    IfxAsclin_Asc_isrTransmit(&g_AsclinAsc.drivers.asc);
 }
 
 ISR2(TimerISR)
 {
-	osEE_tc_stm_set_sr0_next_match( 1000000U );
+    osEE_tc_stm_set_sr0_next_match( 1000000U );
     static long c = 0;
     printfSerial("\n%4ld: ", c++);
 }
 
 TASK(Task1)
 {
-	printfSerial("Task1 Begins...");
-	mdelay(3000);
-	printfSerial("Task1 Finishes...");
+    printfSerial("Task1 Begins...");
+    mdelay(3000);
+    printfSerial("Task1 Finishes...");
 
-	TerminateTask();
+    TerminateTask();
 }
 
 TASK(Task2)
 {
-	printfSerial("Task2 Begins...");
-	mdelay(3000);
-	printfSerial("Task2 Finishes...");
+    printfSerial("Task2 Begins...");
+    mdelay(3000);
+    printfSerial("Task2 Finishes...");
 
-	TerminateTask();
+    TerminateTask();
 }
--- 06. Task Activation/master.c
+++ 06. Task Activation/master.c
@@ -5,33 +5,33 @@ extern App_AsclinAsc g_AsclinAsc;
 
 ISR(asclin0TxISR)
 {
-	IfxAsclin_Asc_isrTransmit(&g_AsclinAsc.drivers.asc);
+    IfxAsclin_Asc_isrTransmit(&g_AsclinAsc.drivers.asc);
 }
 
 
 ISR2(TimerISR)
 {
-	osEE_tc_stm_set_sr0_next_match( 1000000U );
+    osEE_tc_stm_set_sr0_next_match( 1000000U );
     static long c = -4;
     if (c == 0)
-    	ActivateTask(Task1);
+        ActivateTask(Task1);
     printfSerial("\n%4ld: ", c++);
 }
 
 TASK(Task1)
 {
-	printfSerial("Task1 Begins...");
-	mdelay(3000);
-	ActivateTask(Task2);
-	mdelay(3000);
-	printfSerial("Task1 Finishes...");
-	TerminateTask();
+    printfSerial("Task1 Begins...");
+    mdelay(3000);
+    ActivateTask(Task2);
+    mdelay(3000);
+    printfSerial("Task1 Finishes...");
+    TerminateTask();
 }
 
 TASK(Task2)
 {
-	printfSerial("Task2 Begins...");
-	mdelay(3000);
-	printfSerial("Task2 Finishes...");
-	TerminateTask();
+    printfSerial("Task2 Begins...");
+    mdelay(3000);
+    printfSerial("Task2 Finishes...");
+    TerminateTask();
 }
