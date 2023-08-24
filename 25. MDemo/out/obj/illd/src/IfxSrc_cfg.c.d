1adb775b5b8b6fe0630da80f7d77ee7b1d222184
changjjo <whchangjo@gmail.com>
2023-08-25 01:43:10 +0900

    ngv-rtos

--- 25. MDemo/out/obj/illd/src/IfxSrc_cfg.c.d
+++
@@ -1,5 +0,0 @@
-obj/illd/src/IfxSrc_cfg.c.o: \
- /cygdrive/c/Users/Auto/Desktop/RTOS-W~1/ngv-rtos/25E35F~1.MDE/illd/src/IfxSrc_cfg.c \
- /cygdrive/c/Users/Auto/Desktop/RTOS-W~1/ngv-rtos/25E35F~1.MDE/illd/Libraries/iLLD/TC27D/Tricore/_Impl/IfxSrc_cfg.h
-
-/cygdrive/c/Users/Auto/Desktop/RTOS-W~1/ngv-rtos/25E35F~1.MDE/illd/Libraries/iLLD/TC27D/Tricore/_Impl/IfxSrc_cfg.h:
