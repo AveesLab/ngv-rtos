3d5b36f12c9e035e75a5b82c1f322fa3cc20443b
solahn <solahn00@gmail.com>
2025-04-08 09:31:28 +0900

    Add 06-3 Example

---
+++ 06-3. Periodic Tasks/asw.c
@@ -0,0 +1,27 @@
+#include "bsw.h"
+
+ISR2(TimerISR)
+{
+    static long c = -4;
+    osEE_tc_stm_set_sr0_next_match(1000000U);
+    if (c % 10 == 0)
+        ActivateTask(Task1);
+    printfSerial("\n%4ld: ", c++);
+}
+
+TASK(Task1)
+{
+    printfSerial("Task1 Begins...");
+    mdelay(3000);
+    ActivateTask(Task2);
+    printfSerial("Task1 Finishes...");
+    TerminateTask();
+}
+
+TASK(Task2)
+{
+    printfSerial("Task2 Begins...");
+    mdelay(4000);
+    printfSerial("Task2 Finishes...");
+    TerminateTask();
+}
